c93af2e3f7b8211068b82800f1490b95dffed702
Jazz <webmaster@codej.ru>
2024-05-07 07:47:41 +0500

    Create flashed messages

--- codej/main/views.py
+++ codej/main/views.py
@@ -3,13 +3,16 @@ import os
 from starlette.responses import FileResponse
 
 from ..dirs import static
+from ..common.flashed import get_flashed, set_flashed
 from ..common.pg import get_conn
 
 
 async def show_index(request):
+    await set_flashed(request, 'This is a flashed message again.')
     return request.app.jinja.TemplateResponse(
         'main/index.html',
         {'request': request,
+         'flashed': await get_flashed(request),
          'listed': True})
 
 
--- codej/static/js/main/index.js
+++ codej/static/js/main/index.js
@@ -3,5 +3,6 @@ $(function() {
   let dt = luxon.DateTime.now();
   formatFooter(dt);
   if ($('.today-field').length) renderTF('.today-field', dt);
+  $('body').on('click', '.closeable', closeTopFlashed);
   checkMC(860);
 });
